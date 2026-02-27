967415f73697270f7ae59b0003d7efbcb634b150
GuiLuiz27 <guiluiz.oliveirasilva@gmail.com>
2026-02-27 14:40:29 -0300

    aula7-2

---
+++ src/sobre/page.jsx
@@ -0,0 +1,10 @@
+
+function Sobre () {
+    return(
+        <div>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nam pariatur cum et odit eaque veritatis officia possimus suscipit eligendi! Sequi esse amet distinctio voluptatibus, eius recusandae magnam explicabo doloribus!</h1>
+        </div>
+    )
+}
+
+export default Sobre;
