7dae126267b54b5337a2ab63c4f83dcdc3d6422e
GuiLuiz27 <guiluiz.oliveirasilva@gmail.com>
2026-03-06 14:42:33 -0300

    aula06/03

---
+++ src/pages/exemplos/ex-02/index.jsx
@@ -0,0 +1,16 @@
+import styles from './import.module.css';
+
+function Exemplo02()  {
+    return(
+        <div className={styles.containerEx2}>
+            <h1>Exemplo 2</h1>
+            <h2>Estilização de css</h2>
+            <h3>Uso de Css em difrentes componentes</h3>
+            <p>Eu não fiz nada</p>
+            <label>Exemplo label</label>
+            <span>Exemplo Span</span>
+        </div>
+    )
+}
+
+export default Exemplo02;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,7 +3,6 @@ function Home()  {
     return(
         <div>
             <h1>Aula de React com Vite JS</h1>
-            <h3>Estilização css</h3>
             <p>Eu não fiz nada</p>
         </div>
     )
