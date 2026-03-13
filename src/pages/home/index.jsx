be8289f01123f30879576b8a0f5dbea334de4ad8
GuiLuiz27 <guiluiz.oliveirasilva@gmail.com>
2026-03-13 14:40:24 -0300

    13/03

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,23 @@
 
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router";
+
 function Home()  {
+
+    const navigate = useNavigate();
+
     return(
         <div>
             <h1>Aula de React com Vite JS</h1>
-            <p>Eu não fiz nada</p>
+
+            <button
+             onClick={() => navigate('/exemplo/1')}
+             >
+            Exemplo1
+            </button>
+
+            <Link to="/exemplo/2">Exemplo 2</Link>
+
         </div>
     )
 }
