337d747eb47f2e4ecb819c9f1fbf1f567adc6b2b
GuiLuiz27 <guiluiz.oliveirasilva@gmail.com>
2026-03-17 11:40:06 -0300

    17/03

--- src/pages/exemplos/ex-01/index.jsx
+++ src/pages/exemplos/ex-01/index.jsx
@@ -1,12 +1,10 @@
-
 function Exemplo01() {
-    return(
+    return (
         <>
             <h1>Exemplo 1</h1>
             <p>Conteúdo mínimo para componente React</p>
         </>
     )
 }
-
 
 export default Exemplo01;
