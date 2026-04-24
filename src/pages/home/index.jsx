336baca1ad3f68b9c606de0cabff1e227beb5cad
GuiLuiz27 <guiluiz.oliveirasilva@gmail.com>
2026-04-24 15:04:22 -0300

    24/04

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,7 +11,8 @@
             <h2>Exemplos</h2>       
             <Link to="/exemplo/1">Exemplo 1 - Componente básico</Link>
             <Link to="/exemplo/2">Exemplo 2 - Uso de module CSS</Link>
-            <Link to="/exemplo/3">Exemplo 3 - Contador 10</Link>            
+            <Link to="/exemplo/3">Exemplo 3 - Contador 10</Link>
+            <Link to="/exemplo/4">Exemplo 4 - Contador 10</Link>              
           </div>
           
           <div className={styles.containerCards}>    
